d2205fe25385587c582586315bc1c34acf0f0dcd
MAHMOUD MAMDOH SOLIMAN <111503676+MAHMOUDMAMDOH8@users.noreply.github.com>
2024-04-15 02:48:25 +0200

    Add files via upload

---
+++ Mage pipeline/transformers/change_data_typt.py
@@ -0,0 +1,36 @@
+import pandas as pd
+if 'transformer' not in globals():
+    from mage_ai.data_preparation.decorators import transformer
+if 'test' not in globals():
+    from mage_ai.data_preparation.decorators import test
+
+
+@transformer
+def transform(data, *args, **kwargs):
+    uber_dtype = {
+        'VendorID': pd.Int64Dtype(),
+        'passenger_count': pd.Int64Dtype(),
+        'trip_distance': float,
+        'RatecodeID': pd.Int64Dtype(),
+        'store_and_fwd_flag': str,
+        'pickup_longitude': float,  
+        'pickup_latitude': float,   
+        'payment_type': pd.Int64Dtype(),
+        'fare_amount': float,
+        'extra': float,
+        'mta_tax': float,
+        'tip_amount': float,
+        'tolls_amount': float,
+        'improvement_surcharge': float,
+        'total_amount': float
+    }
+    data = data.astype(uber_dtype)
+    return data
+
+
+@test
+def test_output(output, *args) -> None:
+    """
+    Template code for testing the output of the block.
+    """
+    assert output is not None, 'The output is undefined'
